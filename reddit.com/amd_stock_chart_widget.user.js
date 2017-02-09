961da1e9cde188207b8b8de1ee2393052b0b3c8a
Lukas S <Dijky@users.noreply.github.com>
2017-02-09 22:34:28 +0100

    Putting it all into an anonymous function

--- reddit.com/amd_stock_chart_widget.user.js
+++ reddit.com/amd_stock_chart_widget.user.js
@@ -9,60 +9,62 @@
 // @grant       none
 // ==/UserScript==
 
-function payload(document, window)
-{
-	console.log(">> dijky.reddit.stockChart is active");
-
-	var targetNode = document.getElementsByClassName("side")[0];
-	var nextNode = targetNode.getElementsByClassName("spacer")[3];
-	
-	var chartNode = document.createElement("div");
-	chartNode.id = "dijky-stock-chart-widget";
-	chartNode.classList.add("spacer");
-	chartNode.style.width = "100%";
-	targetNode.insertBefore(chartNode, nextNode);
-	
-	var orange = "rgba(241, 79, 37, 1)";
-	
-	var chartWidgetOptions = {
-		container_id: "dijky-stock-chart-widget",
-		width: "100%",
-		tabs: [ "Stock" ],
-		symbols: {
-			"Stock": ["AMD", "Nvidia", "Intel"]
-		},
-		symbols_description: {
-			"AMD": "NASDAQ:AMD",
-			"Nvidia": "NASDAQ:NVDA",
-			"Intel": "NASDAQ:INTC"
-		},
-		fontColor: orange,
-		trendLineColor: orange,
-		underLineColor: "rgba(241, 79, 37, 0.08)",
-		activeTickerBackgroundColor: "rgba(241, 79, 37, 0.4)",
-		styleTabActiveBorderColor: orange,
-		styleTickerSymbolColor: "#000000",
-		styleTickerChangeUpColor: "",
-		// styleTickerChangeDownColor: "#000000", 
-		styleWidgetNoBorder: true
-	};
-	
-	var scriptNode = document.createElement("script");
-	scriptNode.setAttribute("type", "text/javascript");
-	document.head.appendChild(scriptNode);
-	
-	scriptNode.onload = function()
+(function(){
+	function run(document, window)
 	{
-		// console.log("TradingView JS loaded");
-		// Create widget
-		var chartWidget = new TradingView.MiniWidget(chartWidgetOptions);
+		console.log(">> dijky.reddit.stockChart is active");
+
+		var targetNode = document.getElementsByClassName("side")[0];
+		var nextNode = targetNode.getElementsByClassName("spacer")[3];
+		
+		var chartNode = document.createElement("div");
+		chartNode.id = "dijky-stock-chart-widget";
+		chartNode.classList.add("spacer");
+		chartNode.style.width = "100%";
+		targetNode.insertBefore(chartNode, nextNode);
+		
+		var orange = "rgba(241, 79, 37, 1)";
+		
+		var chartWidgetOptions = {
+			container_id: "dijky-stock-chart-widget",
+			width: "100%",
+			tabs: [ "Stock" ],
+			symbols: {
+				"Stock": ["AMD", "Nvidia", "Intel"]
+			},
+			symbols_description: {
+				"AMD": "NASDAQ:AMD",
+				"Nvidia": "NASDAQ:NVDA",
+				"Intel": "NASDAQ:INTC"
+			},
+			fontColor: orange,
+			trendLineColor: orange,
+			underLineColor: "rgba(241, 79, 37, 0.08)",
+			activeTickerBackgroundColor: "rgba(241, 79, 37, 0.4)",
+			styleTabActiveBorderColor: orange,
+			styleTickerSymbolColor: "#000000",
+			styleTickerChangeUpColor: "",
+			// styleTickerChangeDownColor: "#000000", 
+			styleWidgetNoBorder: true
+		};
+		
+		var scriptNode = document.createElement("script");
+		scriptNode.setAttribute("type", "text/javascript");
+		document.head.appendChild(scriptNode);
+		
+		scriptNode.onload = function()
+		{
+			// console.log("TradingView JS loaded");
+			// Create widget
+			var chartWidget = new TradingView.MiniWidget(chartWidgetOptions);
+		}
+		scriptNode.setAttribute("src", "https://s3.amazonaws.com/tradingview/tv.js");
 	}
-	scriptNode.setAttribute("src", "https://s3.amazonaws.com/tradingview/tv.js");
-}
 
-function handler()
-{
-	payload(document, window);
-}
+	function handler()
+	{
+		run(document, window);
+	}
 
-window.addEventListener("load", handler);
+	window.addEventListener("load", handler);
+})(document, window);
