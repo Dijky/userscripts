69405e62b6029b0519552364ef05f68fd82e82af
Lukas S <Dijky@users.noreply.github.com>
2017-02-10 09:26:40 +0100

    Version 6: Configuration UI Wohooo

--- reddit.com/amd_stock_chart_widget.user.js
+++ reddit.com/amd_stock_chart_widget.user.js
@@ -1,41 +1,63 @@
 // ==UserScript==
-// @name        AMD Stock Chart Widget
-// @namespace   https://github.com/dijky/userscripts/reddit.com
+// @name		AMD Stock Chart Widget
+// @namespace	https://github.com/dijky/userscripts/reddit.com
 // @author		Dijky
-// @description Add a stock chart widget to the sidebar of AMD subreddits. Widget powered by TradingView.com
+// @description	Add a stock chart widget to the sidebar of AMD subreddits. Widget powered by TradingView.com
 // @downloadURL	https://github.com/Dijky/userscripts/raw/master/reddit.com/amd_stock_chart_widget.user.js
 // @supportURL	https://github.com/Dijky/userscripts/issues
-// @include     /^https?://\w+\.reddit\.com/r/Amd/.*$/
-// @include     /^https?://\w+\.reddit\.com/r/AyyMD/.*$/
-// @include     /^https?://\w+\.reddit\.com/r/AMDHelp/.*$/
-// @include     /^https?://\w+\.reddit\.com/r/AMD_Stock/.*$/
-// @include     /^https?://\w+\.reddit\.com/r/radeon/.*$/
-// @include     /^https?://\w+\.reddit\.com/r/AdvancedMicroDevices/.*$/
-// @version     5
-// @grant       none
+// @require		https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
+// @require		https://s3.amazonaws.com/tradingview/tv.js
+// @include		/^https?://\w+\.reddit\.com/r/Amd/.*$/
+// @include		/^https?://\w+\.reddit\.com/r/AyyMD/.*$/
+// @include		/^https?://\w+\.reddit\.com/r/AMDHelp/.*$/
+// @include		/^https?://\w+\.reddit\.com/r/AMD_Stock/.*$/
+// @include		/^https?://\w+\.reddit\.com/r/radeon/.*$/
+// @include		/^https?://\w+\.reddit\.com/r/AdvancedMicroDevices/.*$/
+// @version		6
+// @grant		GM_getValue
+// @grant		GM_setValue
+// @grant		GM_addStyle
+// @grant		GM_getMetadata
+// @grant		GM_registerMenuCommand
 // ==/UserScript==
 
-(function(document, window, options){
-	function run(document, window)
+(function(document, window){
+	var chartNode;
+	var chartWidget;
+	var configuration;
+	
+	function buildWidget(chartNode)
 	{
+		while(chartNode.firstChild)
+		{
+			chartNode.removeChild(chartNode.firstChild);
+		}
+		
+		var bigmode = configuration.get("mode") == "big";
+		var range = configuration.get("range");
+		var _symbols = configuration.get("symbols");
 		var orange = function(a) {return "rgba(241, 79, 37, " + a + ")"; };
 		var symbols = [];
-		for (var k in options.symbols)
+		for (var k = 0; k < _symbols.length; ++k)
 		{
-			if (options.symbols.hasOwnProperty(k))
+			if (_symbols[k].indexOf("//") == 0)
 			{
-				var s = options.symbols[k];
-				if (s.indexOf("|") == -1)
-				{
-					s += "|" + options.range;
-				}
-				symbols.push([k, s]);
+				continue;
 			}
+			var d = _symbols[k].split("=");
+			var s = d[1];
+			
+			if (s.indexOf("|") == -1)
+			{
+				s += "|" + range;
+			}
+			
+			symbols.push([d[0], s]);
 		}
 		var chartWidgetOptions = {
 			container_id: "dijky-stock-chart-widget",
 			width: "100%",
-			height: options.bigmode ? "500px" : ((210+symbols.length*31) + "px"),
+			height: bigmode ? "500px" : ((210+symbols.length*31) + "px"),
 			chartOnly: false,
 			symbols: symbols,
 			fontColor: orange(1),
@@ -49,56 +71,80 @@
 			styleWidgetNoBorder: true
 		};
 	
-		console.log(">> dijky/reddit.com/amd_stock_chart_widget is active");
+		console.log(">> dijky/reddit.com/amd_stock_chart_widget is building");
 
+		// Create widget
+		var widget;
+		if (bigmode)
+		{
+			widget = new TradingView.MediumWidget(chartWidgetOptions);
+		}
+		else
+		{
+			widget = new TradingView.MiniWidget(chartWidgetOptions);
+		}
+		return widget;
+	}
+	
+	function buildConfig()
+	{
+		var cfg = {
+			title: "Dijky's AMD Stock Chart Widget Configuration",
+			parameters: {
+				"mode": {
+					type: "select",
+					choices: ["small", "big"],
+					variant: "radio",
+					"default": "small"
+				},
+				"range": {
+					label: "Default date range",
+					type: "select",
+					choices: ["1d", "1m", "3m", "1y", "5y", "max"],
+					"default": "1m"
+				},
+				"symbols": {
+					type: "custom",
+					html: '<textarea id="dijky-stock-chart-widget-cfg-symbols"></textarea>',
+					set: function(value, parent) {
+						parent.querySelectorAll("textarea")[0].value = value.join("\n");
+					},
+					get: function(parent) {
+						return parent.querySelectorAll("textarea")[0].value.split("\n");
+					},
+					"default": [
+						"AMD=NASDAQ:AMD",
+						"Nvidia=NASDAQ:NVDA",
+						"Intel=NASDAQ:INTC"
+					]
+				}
+			},
+			onSave: function(values) {
+				chartWidget = buildWidget(chartNode);
+			}
+		};
+		return new MonkeyConfig(cfg);
+	}
+
+	function handler()
+	{
+		configuration = buildConfig();
+		GM_registerMenuCommand("AMD Stock Chart Widget Config", function ()
+		{
+			configuration.open("layer");
+		});
+		
 		var targetNode = document.getElementsByClassName("side")[0];
 		var nextNode = targetNode.getElementsByClassName("spacer")[4];
 		
-		var chartNode = document.createElement("div");
+		chartNode = document.createElement("div");
 		chartNode.id = "dijky-stock-chart-widget";
 		chartNode.classList.add("spacer");
 		chartNode.style.width = "100%";
 		targetNode.insertBefore(chartNode, nextNode);
 		
-		var scriptNode = document.createElement("script");
-		scriptNode.setAttribute("type", "text/javascript");
-		document.head.appendChild(scriptNode);
-		
-		scriptNode.onload = function()
-		{
-			// console.log("TradingView JS loaded");
-			// Create widget
-			var chartWidget;
-			if (options.bigmode)
-			{
-			    chartWidget = new TradingView.MediumWidget(chartWidgetOptions);
-			}
-			else
-			{
-					chartWidget = new TradingView.MiniWidget(chartWidgetOptions);
-			}
-		}
-		scriptNode.setAttribute("src", "https://s3.amazonaws.com/tradingview/tv.js");
+		chartWidget = buildWidget(chartNode);
 	}
 
-	function handler()
-	{
-		run(document, window);
-	}
-
-	window.addEventListener("load", handler);
-})(document, window, {
-	// Configuration
-	bigmode: false, // Enable big mode: true or false
-	range: "3m",    // Chart range for all symbols: 1d, 1m, 3m, 1y, 5y
-	symbols: {
-		// List of symbols in the format
-		// "display name": "symbol name",
-		"AMD": "NASDAQ:AMD",
-		// "AMD 1y": "NASDAQ:AMD|1y", // You can override the chart range with |1d or |1y etc
-		"Intel": "NASDAQ:INTC",
-		"Nvidia": "NASDAQ:NVDA",
-		// "SOXX": "NASDAQ:SOXX",
-		// "NASDAQ Comp": "NASX",
-	}
-});
+	handler();
+})(document, window);
